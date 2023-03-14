173d4b7402981177800ade705bf36acaa9d7a167
CalebHaleDev <107201448+CalebHaleDev@users.noreply.github.com>
2023-03-14 12:17:09 -0600

    added a few notes, more outline/pseudocode

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ console.log("Hello World");
 
 //this is just to collapse visually
 function notes(){
-var exampleVariable = 'something';  //can be used anywhere
-let otherExampleVariable = 'something else';  //can only be used in scope
+var exampleVariable = 'something';  //can be used anywhere, can declare multiple in the same scope
+let otherExampleVariable = 'something else';  //can only be used in scope, can't declare multiple times in same scope
 const pi = 3.1415;
 //strings, numbers, and booleans are the only primitive dataTypes
 //JS is dynamic, so the data types can change at runtime
@@ -31,6 +31,11 @@ let emptyArray = [];
 function exampleFunction() {
     //function body
 }
+
+//the or operator || can be used to pass in a default value in a return statement if another value doesn't exist
+//tertiary operation is condition ? statement-if-true : statement-if-false;
+//"use strict"; does checking for common mistakes
+
 }
 
 function makeGrid(width, height){
@@ -45,48 +50,61 @@ function makeGrid(width, height){
     console.log("grid made");
     console.log(gameGrid);
 }
-
 function printGrid(){
     gameGrid.forEach(element => {
         console.log(element);
     });
 }
+function gameSetup(){
+    level = 2;
+    makeGrid(level+1,5);
+    printGrid();
+    fallerCoords = (0,0);
+}
+function doGravity(){
+    //move blocks down
+    //if faller lands, create new faller and check for rows
+    //if the faller lands on the first row (reaches the top), level = 0 (which ends the game)
+    //set gravityTimer = now + (1.1-.1*difficulty) sec (minimum .25 sec)
+}
+function shiftFaller(direction){
+    //if direction is not 0, set newlocation to fallerCoord +- direction, otherwise new location is downward
+    /*if(newlocation is open){
+        set the newlocation to the faller
+        set the old location to blank
+        change the faller coordinates
+        //if moving downward, increase score?
+        //set shiftTimer = now + difficulty/20 sec (only if shift successful?)
+    }*/
+    //set shiftTimer = now + difficulty/20 sec (only if shift successful?)
+}
 
-makeGrid(3,5);
-printGrid();
+
+gameSetup();
 
 
-
-
-
-var i=0;
 console.log("test");
+//main game loop
 var gameloopID = setInterval(()=> {
-    console.log("doing ");
-    i++;
-    if(i>1){
+
+/*
+ * if (gravityTimer < now){
+ *  doGravity();
+ * }
+ * 
+ * if (userTime < now && keypressed){
+ * tertiary for direction based on keypressed
+ *  shiftFaller(direction)
+ * }
+ * 
+ */
+
+
+    console.log("doing main loop");
+    if(true/*endCondition*/){
         clearInterval(gameloopID);
     }
-},100);
-
-
-
+},100); //change rate to 5 later
 
 //wait(1000);
 
-
-      //main game loop
-/*
- * if (gravityTime < now){
- * move blocks (if landed, create new faller)   //if landed in the first row, end game (level = 0)
- * check for rows
- * reset timer    gravityTime = now + 1.1-.1*difficulty sec
- * }
- * 
- * if (userTime < now){
- * shift if possible      user input
- *  if shift successful, reset timer    userTime = now + difficulty/20 sec
- * }
- * 
- * sleep 5+ milliseconds
- */
