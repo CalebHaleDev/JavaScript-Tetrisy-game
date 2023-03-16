2786e6f7d27fcb938fee1f5e7f46ae024dc9d0ef
CalebHaleDev <107201448+CalebHaleDev@users.noreply.github.com>
2023-03-16 10:40:50 -0600

    organized settings for easy adjustment, created gravity function
    setting the value of a square affects all items of that index across all arrays in the 2d array (the whole column)

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 console.log("Hello World");
 
-//this is just to collapse visually
+//this is in a function just to collapse visually
 function notes(){
 var exampleVariable = 'something';  //can be used anywhere, can declare multiple in the same scope
 let otherExampleVariable = 'something else';  //can only be used in scope, can't declare multiple times in same scope
@@ -35,37 +35,84 @@ function exampleFunction() {
 //the or operator || can be used to pass in a default value in a return statement if another value doesn't exist
 //tertiary operation is condition ? statement-if-true : statement-if-false;
 //"use strict"; does checking for common mistakes
+//wait(1000);       //why doesn't wait work?
 
 }
 
 function makeGrid(width, height){
     gameGrid = [];
-    var row = [];
+    emptyRow = [];
     for(var i=0;i<width;i++){
-        row.push(0);
+        emptyRow.push(emptySpaceCharacter);
     }
     for(var i=0;i<height;i++){
-        gameGrid.push(row);
+        gameGrid.push(emptyRow);
     }
     console.log("grid made");
-    console.log(gameGrid);
+    //console.log(gameGrid);
 }
 function printGrid(){
     gameGrid.forEach(element => {
         console.log(element);
     });
+    console.log("btw, now is "+(Date.now()%10000));
 }
 function gameSetup(){
-    level = 2;
-    makeGrid(level+1,5);
+    //settings:
+    let startingLevel = 2;
+    let unscaledWidth = 1;
+    let unscaledHeight = 5;
+    emptySpaceCharacter = "_";
+    //initialize game
+    level = startingLevel;
+    makeGrid(level+unscaledWidth, level+unscaledHeight);
+    //gameGrid[0][0] = 1;
     printGrid();
-    fallerCoords = (0,0);
+    fallerCoords = [0,0];
+    gravityTimer=Date.now()+Math.min((1100-(100*level)),250);
+    shiftTimer=Date.now()+(1000*level/20);
+    console.log("starting game loop");
+    console.log("now is "+Date.now());
+    console.log("gravityTimer is "+gravityTimer);
+    console.log("shiftTimer is "+shiftTimer);
 }
 function doGravity(){
-    //move blocks down
-    //if faller lands, create new faller and check for rows
-    //if the faller lands on the first row (reaches the top), level = 0 (which ends the game)
-    //set gravityTimer = now + (1.1-.1*difficulty) sec (minimum .25 sec)
+    //move everything down
+    for(let column = gameGrid[0].length; column>0; column--){
+        for(let row = gameGrid.length-1; row>0; row--){
+            if(gameGrid[row][column]==emptySpaceCharacter){
+                gameGrid[row][column] = gameGrid[row-1][column];
+                gameGrid[row-1][column] = emptySpaceCharacter;
+            }
+        }
+    }
+    fallerCoords[1]++;
+
+    //if the faller lands
+    if(fallerCoords[1]==gameGrid.length-1 || gameGrid[fallerCoords[1]+1][fallerCoords[0]]!=emptySpaceCharacter){
+        //check for game over
+        if(fallerCoords[1]=0){
+            level = 0;
+            return;     //not necessary, but skips some redundant lines
+        }
+        
+        //make new faller
+        gameGrid[0][1] = 1; //replace the 1 with whatever is falling
+        fallerCoords = [0,0];
+
+        //check for rows made. This could be changed to only check the row the faller landed in in the future for efficiency
+        gameGrid.forEach(element => {
+            if(element.indexOf(emptySpaceCharacter)==-1){   //if there's no empty space in the row
+                console.log("row made: "+element);
+                gameGrid[gameGrid.indexOf(element)] = emptyRow; //clear row
+                //could also set the row to a "win row" to show visually, print out the grid once to make sure it displays, then wipe it, just for a fun visual
+            }
+        });
+    }
+    console.log("faller is at "+fallerCoords);
+    
+    gravityTimer=Date.now()+Math.min((1100-(100*level)),250);
+    level -= .02;
 }
 function shiftFaller(direction){
     //if direction is not 0, set newlocation to fallerCoord +- direction, otherwise new location is downward
@@ -82,15 +129,27 @@ function shiftFaller(direction){
 
 gameSetup();
 
-
-console.log("test");
 //main game loop
 var gameloopID = setInterval(()=> {
 
+    if(gravityTimer<Date.now()){
+    doGravity();
+    printGrid();
+}
+
 /*
- * if (gravityTimer < now){
- *  doGravity();
- * }
+this.addEventListener('keypress', event => {
+    if (event.code == 13) {
+      alert('hi.')
+    }
+  })
+*/
+let keypressed = 0;
+if(shiftTimer<Date.now()&& keypressed!=null){
+    //stuff below
+}
+/*
+
  * 
  * if (userTime < now && keypressed){
  * tertiary for direction based on keypressed
@@ -100,11 +159,10 @@ var gameloopID = setInterval(()=> {
  */
 
 
-    console.log("doing main loop");
-    if(true/*endCondition*/){
+    //console.log("doing main loop");
+    if(level<1){
         clearInterval(gameloopID);
     }
 },100); //change rate to 5 later
 
-//wait(1000);
-
+console.log("game ended");
