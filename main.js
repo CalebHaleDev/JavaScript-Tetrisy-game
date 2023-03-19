099f2918d46b5b57737795a80490cd3b85d52462
CalebHaleDev <107201448+CalebHaleDev@users.noreply.github.com>
2023-03-18 22:10:40 -0600

    temporarily undoing extra gravity shift to troubleshoot ghost-fallers. Not the cause

--- main.js
+++ main.js
@@ -55,6 +55,16 @@ function printGrid(){
     print("completed: "+completedRows);
     //print("faller is at "+fallerCoords+"<br>");
 }
+function mainGravityShift(){
+    for(let column = gameGrid[0].length; column>-1; column--){
+        for(let row = gameGrid.length-1; row>0; row--){
+            if(gameGrid[row][column]==emptySpaceCharacter){
+                gameGrid[row][column] = gameGrid[row-1][column];
+                gameGrid[row-1][column] = emptySpaceCharacter;
+            }
+        }
+    }
+}
 function doGravity(){
     //if the faller is landing at the top row, end game immediately
     if(fallerCoords[1]==0 && gameGrid[fallerCoords[1]+1][fallerCoords[0]]!=emptySpaceCharacter){
@@ -62,6 +72,7 @@ function doGravity(){
         return;     //not necessary, but skips some redundant lines
         }
     //move everything down
+    //mainGravityShift();
     for(let column = gameGrid[0].length; column>-1; column--){
         for(let row = gameGrid.length-1; row>0; row--){
             if(gameGrid[row][column]==emptySpaceCharacter){
@@ -74,21 +85,22 @@ function doGravity(){
 
     //if the faller lands
     if(fallerCoords[1]==gameGrid.length-1 || gameGrid[fallerCoords[1]+1][fallerCoords[0]]!=emptySpaceCharacter){
-        //make new faller
-        setFaller(Math.floor(Math.random()*(gameGrid[0].length)), elementList[Math.floor(Math.random()*(level+1))] );
-
         //check for rows made. This could be changed to only check the row the faller landed in in the future for efficiency
         gameGrid.forEach(element => {
             if(element.indexOf(emptySpaceCharacter)==-1){   //if there's no empty space in the row
                 print("row made: "+element);
                 gameGrid[gameGrid.indexOf(element)] = emptyRow.map(x => x); //clear row
-                //could also set the row to a "win row" to show visually, print out the grid once to make sure it displays, then wipe it, just for a fun visual
+                printGrid();
+                //mainGravityShift(); //do an extra shift so the rows fall before a new block starts falling, otherwise it might fall on a floating tower... :|
                 score+=5;
                 if(!completedRows.includes(element)){   //complete a new type of row
                     completedRows.push(element.map(x => x));
                     score+=5;
                 }
             }
+            
+        //make new faller
+        setFaller(Math.floor(Math.random()*(gameGrid[0].length)), elementList[Math.floor(Math.random()*(level+1))] );
         });
     }
     gravityTimer=Date.now()+Math.min((1100-(50*level)),500);
@@ -100,7 +112,7 @@ function shiftFaller(direction){
         gameGrid[newLocation[1]][newLocation[0]] = gameGrid[fallerCoords[1]][fallerCoords[0]];  //set the newLocation to the faller
         gameGrid[fallerCoords[1]][fallerCoords[0]] = emptySpaceCharacter;       //and set the faller to a blank space
         fallerCoords = [newLocation[0],newLocation[1]];
-        //if(direction==0) score++;
+        if(direction==0) score++;
     }
     printGrid();
     shiftTimer=Date.now()+Math.max(1000*(level/20),250); //only if shift successful?
